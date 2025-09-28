61d57de78a3b44b92148a4ece26226318bb856fe
Fikiri-Solutions <info@fikirisolutions.com>
2025-09-28 12:49:50 -0400

    Restore full app with all routes
    - Switched main.tsx back to import App.tsx instead of App.incremental.tsx
    - LandingPage is now working with all imports fixed
    - Full routing and all pages should now be accessible

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import * as Sentry from '@sentry/react'
-import App from './App.incremental'
+import App from './App'
 import './index.css'
 // import { initializeCacheInvalidation } from './utils/cacheInvalidation'
 
